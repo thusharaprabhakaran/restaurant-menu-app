8e70e1bdd2271f59a3a23b30fc823ae9e76d0671
Arun <113289918+thusharaprabhakaran@users.noreply.github.com>
2022-12-06 16:51:55 +0100

    committing project files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+//  ---------To learn how hooks concept works uncomment the below--------
+// import UseState from "./component/Hooks/useState";
+// import UseEffect from "./component/Hooks/useEffect";
+//import UseReducer from "./component/Hooks/useReducer";
+import Resturant from "./component/Basics/Resturant";
+
+const App = () => {
+
+  return <Resturant />;
+  // ------uncomment the below to call hooks componenets---------
+  //  return <UseState />;
+  //  return <UseEffect />;
+  //  return <UseReducer />;
+};
+
+export default App;
